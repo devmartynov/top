d96d15adf72c962b9ad5aa280d5122f41f595d6b
dmartynov <denis.martynov.93@mail.ru>
2023-05-14 12:20:50 +0300

    update models

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,8 +1,16 @@
-export class ReviewModel {
-    _id: string;
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ReviewModel extends Base {}
+
+export class ReviewModel extends TimeStamps {
+    @prop()
     name: string;
+    @prop()
     title: string;
+    @prop()
     description: string;
+    @prop()
     rating: number;
-    createdAt: Date;
 }
