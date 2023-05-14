a0e8079dc12bbbce77b0f415b66659bd68d4da2f
dmartynov <denis.martynov.93@mail.ru>
2023-05-14 12:54:56 +0300

    add review service

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,5 +1,6 @@
 import { prop } from '@typegoose/typegoose';
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { Types } from 'mongoose';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ReviewModel extends Base {}
@@ -13,4 +14,6 @@ export class ReviewModel extends TimeStamps {
     description: string;
     @prop()
     rating: number;
+    @prop()
+    productId: Types.ObjectId
 }
